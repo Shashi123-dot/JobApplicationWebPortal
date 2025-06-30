bea6c5096c9cc092812a58c2f1fd0e2b872ae02f
Shashikant <s.kant23456789@gmail.com>
2025-06-30 23:30:14 +0530

    added CandidateBasicInfo

---
+++ ApplicationWebPortal/Models/CandidateBasicInfo.cs
@@ -0,0 +1,21 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace ApplicationWebPortal.Models
+{
+    public class CandidateBasicInfo
+    {
+        [Required]
+        public string FullName { get; set; }
+
+        [Required, EmailAddress]
+        public string Email { get; set; }
+
+        [Required]
+        [RegularExpression(@"^\d{10}$", ErrorMessage = "Enter valid 10-digit mobile number")]
+        public string Mobile { get; set; }
+
+        public string Location { get; set; }
+
+        public string Experience { get; set; }
+    }
+}
