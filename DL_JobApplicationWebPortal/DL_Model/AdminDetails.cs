1f2674bf16a926f36b060e2ee9fdb2a27d68a05f
Shashikant <s.kant23456789@gmail.com>
2025-07-13 13:58:45 +0530

    Modified admin model details and added admin table in in sql

--- DL_JobApplicationWebPortal/DL_Model/AdminDetails.cs
+++ DL_JobApplicationWebPortal/DL_Model/AdminDetails.cs
@@ -1,8 +1,11 @@
-﻿namespace ApplicationWebPortal.Models
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace ApplicationWebPortal.Models
 {
     public class AdminDetails
     {
-        public int Id { get; set; }
+        [Key]
+        public int UserID { get; set; }
         public string Username { get; set; }
         public string Password { get; set; }
         public string Role { get; set; } // "Admin" or "HR"
